f449211f264ef10e3f3cdd5228f97964e1530c35
Ataba <ahmadata178998@gmail.com>
2024-07-23 12:32:37 +0300

    added animations Home page

--- Cryptocurrency-Dashboard-Project/src/pages/Home.jsx
+++ Cryptocurrency-Dashboard-Project/src/pages/Home.jsx
@@ -27,17 +27,12 @@ function Home() {
       <Link to="/Dashboard" className="animate-fade-in-up">
         <button
           className={`px-6 py-3 font-semibold rounded-lg shadow-lg transition
-          bg-transparent border-2 border-blue-500 hover:border-blue-700
-          text-blue-500 hover:text-blue-700
+          bg-transparent border-2 animate-border-color
           relative overflow-hidden group
-          ${
-            isDarkMode
-              ? "text-blue-400 hover:text-blue-300"
-              : "text-blue-600 hover:text-blue-800"
-          }
+          ${isDarkMode ? "hover:text-white" : "hover:text-black"}
           `}
         >
-          <span className="relative z-10">Get Started</span>
+          <span className="relative z-10 animate-text-color">Get Started</span>
           <div className="absolute inset-0 h-full w-full transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-out bg-gradient-to-r from-blue-500 to-purple-500 opacity-30"></div>
         </button>
       </Link>
